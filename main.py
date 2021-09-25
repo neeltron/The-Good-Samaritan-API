9fcbf05e2f6f6e94d8df5f7e1ff6d1f1ee883768
Neel Adwani <nprosofficial@gmail.com>
2021-09-25 14:20:52 +0000

    added replit db and output debug

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from flask import Flask, request, jsonify
+from replit import db
+import random
+db.clear()
 app = Flask('app')
 
 @app.route('/')
@@ -7,16 +10,31 @@ def hello_world():
 
 @app.route('/input', methods = ['GET', 'POST'])
 def input():
+  num = random.randint(0, 10000)
   name = request.args.get('name')
   heading = request.args.get('heading')
   desc = request.args.get('desc')
   reward = request.args.get('reward')
-  dict = {
-    "name": name,
-    "heading": heading,
-    "description": desc,
-    "reward": reward
-  }
+  lst = [name, heading, desc, reward]
+  db[str(num)] = lst
+  return "check"
+
+@app.route('/output', methods = ['GET', 'POST'])
+def output():
+  output = []
+  for i in db:
+    name = db[i][0]
+    heading = db[i][1]
+    desc = db[i][2]
+    reward = db[i][3]
+    dict = {
+      "name": name,
+      "heading": heading,
+      "description": desc,
+      "reward": reward
+    }
+    output.append(dict)
+  print(output)
   return jsonify(dict)
 
 app.run(host='0.0.0.0', port=8080)
