d104e0e71640267a308a0af3a2bb55c364b05669
Neel Adwani <nprosofficial@gmail.com>
2021-09-25 13:17:27 +0000

    made an entry point

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 app = Flask('app')
 
 @app.route('/')
 def hello_world():
   return 'Hello, World!'
+
+@app.route('/input', methods = ['GET', 'POST'])
+def input():
+  name = request.args.get('name')
+  heading = request.args.get('heading')
+  desc = request.args.get('desc')
+  reward = request.args.get('reward')
+  dict = {
+    "name": name,
+    "heading": heading,
+    "description": desc,
+    "reward": reward
+  }
+  return jsonify(dict)
 
 app.run(host='0.0.0.0', port=8080)
